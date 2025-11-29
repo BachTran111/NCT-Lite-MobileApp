b7a82d03d2693c3f406b00d82ddad6253938d559
hahaha3708 <minhduc3708@gmail.com>
2025-11-29 10:16:39 +0700

    thêm bottom_sheet_selected.xml + ảnh

---
+++ app/src/main/java/com/example/nct_lite/ui/fragment/BottomSheetSelectedFragment.kt
@@ -0,0 +1,19 @@
+package com.example.nct_lite.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nct_lite.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetSelectedFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet_selected, container, false)
+    }
+}
+
